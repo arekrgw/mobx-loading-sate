b73f7900e0fe4d527690c94cb911dc919e97c695
arekrgw <arkadiusz.pawlak.web@gmail.com>
2021-05-06 18:42:25 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { createAtom } from "mobx";
+
+export class LoadingState {
+  private loadingMap = new Map<string, number>();
+  private atom = createAtom("loadingState");
+  private defaultKeyName;
+
+  constructor(defaultKey?: string) {
+    this.atom = createAtom("loadingState");
+    this.defaultKeyName = defaultKey || "default";
+    this.loadingMap.set(this.defaultKeyName, 0);
+  }
+
+  on = (key?: string) => {
+    const loadingStateValue = this.loadingMap.get(key || this.defaultKeyName);
+
+    this.loadingMap.set(
+      key || this.defaultKeyName,
+      loadingStateValue ? loadingStateValue + 1 : 1
+    );
+    this.atom.reportChanged();
+  };
+
+  is = (key?: string) => {
+    this.atom.reportObserved();
+    return !!this.loadingMap.get(key || this.defaultKeyName);
+  };
+
+  off = (key?: string) => {
+    const loadingStateValue = this.loadingMap.get(key || this.defaultKeyName);
+
+    this.loadingMap.set(
+      key || this.defaultKeyName,
+      loadingStateValue ? loadingStateValue - 1 : 0
+    );
+    this.atom.reportChanged();
+  };
+
+  reset = (key?: string) => {
+    this.loadingMap.set(key || this.defaultKeyName, 0);
+    this.atom.reportChanged();
+  };
+}
